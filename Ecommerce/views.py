f8fd0a9dba0ae6bb19e7668b9126327bbf8d3b9c
anish gharti <anishgharti10@gmail.com>
2023-05-30 15:46:33 +0545

    showing stars in home page

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, ReviewRating
+
 
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
-    context = {
+   products = Product.objects.all().filter(is_available=True)
+   reviews = None
+   for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+
+   context = {
        'products':products,
+            'reviews': reviews,
     }
-    return render(request, 'home.html', context)
+   return render(request, 'home.html', context)
