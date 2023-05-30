63822a13325c8a10c48a6b35b77559a1f76f422b
anish gharti <anishgharti10@gmail.com>
2023-05-30 13:12:03 +0545

    working on edit profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -33,5 +33,23 @@ class RegisterationForm(forms.ModelForm):
         if password!= confirm_password:
             raise forms.ValidationError("password doesnot match")        
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
 
-  
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+# class UserProfileForm(forms.ModelForm):
+#     profile_picture = forms.ImageField(required=False, error_messages = {'invalid':("Image files only")}, widget=forms.FileInput)
+#     class Meta:
+#         model = UserProfile
+#         fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')
+
+#     def __init__(self, *args, **kwargs):
+#         super(UserProfileForm, self).__init__(*args, **kwargs)
+#         for field in self.fields:
+#             self.fields[field].widget.attrs['class'] = 'form-control'
