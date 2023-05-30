152a68c1b7eb881d993f4ff8b0f42aa1462cb2be
anish gharti <anishgharti10@gmail.com>
2023-05-30 14:38:03 +0545

    setup decouple

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)l6cyr-x(mxiv&g+-%$aakr$9k=t@i7mr#cp$+gmkqm7*z6kq'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -137,11 +136,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # smtp configuration
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER = "anishgharti10@gmail.com"
-EMAIL_HOST_PASSWORD = 'velihukweejqlpif'
-EMAIL_USE_TLS = True
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
