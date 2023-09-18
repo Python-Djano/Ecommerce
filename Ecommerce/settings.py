3b979d85ac5319d22a97de79ceda0d1b1459b382
Anish Gharti <anishchengre@kali.anishChengre.com>
2023-09-18 14:29:53 +0545

    added cname to allowed hosts again

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-greatkart-env.eba-wbbxnime.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['anish-ecommerce-env.eba-i3kxe99e.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
