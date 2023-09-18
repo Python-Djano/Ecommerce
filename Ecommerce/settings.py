ec4c9274f08419432bdeae4ed081653d8119fdb8
anish <anishgharti10@gmail.com>
2023-09-18 15:45:09 +0545

    feat:restore

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ['anish-ecommerce-env.eba-i3kxe99e.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['anish-ecommerce-env.eba-i3kxe99e.us-west-2.elasticbeanstalk.com', '172.31.40.190']
 
 
 # Application definition
