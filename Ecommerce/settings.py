20e67ac7280373fd8fe5f75af7c40450bfcd98e6
Anish Gharti <anishchengre@kali.chengre>
2024-03-11 13:13:19 +0545

    final touch

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    'Ecommerce/static',
-]
+# STATICFILES_DIRS = [
+#     'Ecommerce/static',
+# ]
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
